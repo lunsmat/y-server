148c606dd807180886f3ec80b1e161acf6f64e63
Luan Farias <luan.farias@ifpa.edu.br>
2026-08-27 11:19:46 -0300

    Changed name

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import PingController from '@controllers/PingControler';
+import PingController from '@controllers/PingController';
 import UsersController from '@controllers/UsersController';
 
 const routes = Router();
